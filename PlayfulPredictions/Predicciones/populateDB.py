13afbcefed9fa586e1a87ce19b4a6152d6c271e3
pedlopruz <plopezr2011@gmail.com>
2024-03-20 09:51:34 +0100

    Carga Completa

--- PlayfulPredictions/Predicciones/populateDB.py
+++ PlayfulPredictions/Predicciones/populateDB.py
@@ -3,14 +3,17 @@ import urllib.request
 from .models import PartidosEntrenamiento, PartidoReal, PartidoSinPredecir
 import csv
 path = "data/Entrenamiento.csv"
-path = "data/Temporada19-23.csv"
+path2 = "data/Temporada19-23.csv"
 
 def populateDatabaseEntrenamiento():
     PartidosEntrenamiento.objects.all().delete()
     r = cargarPartidoEntrenamiento()
     r18_19 = cargarPartidoEntrenamientoLigaEsp18_19()
     r218_19 = cargarPartidoEntrenamientoLigaEsp2_18_19()
-    return (r,r18_19,r218_19)
+    r19_23 = cargarPartidoEntrenamientoTemporada19_23()
+    ad = cargarDatosEntrenamientoAdicionales()
+    return (r,r18_19,r218_19, r19_23,ad)
+
 
 def populateDatabaseReal():
     PartidoReal.objects.all().delete()
@@ -93,9 +96,6 @@ def cargarPartidoEntrenamientoLigaEsp18_19():
             print(f"Ocurrió un error al procesar la jornada {1}: {e}")
     return "Carga Completada"  
 
-import urllib.request
-from bs4 import BeautifulSoup
-
 def cargarPartidoEntrenamientoLigaEsp2_18_19():
     id = 37529
     temporada = "2018-19"
@@ -152,15 +152,14 @@ def cargarPartidoEntrenamientoLigaEsp2_18_19():
     return "Carga Completada"
   
 def cargarPartidoEntrenamientoTemporada19_23():
+    id = 37991
     pe = None
-    with open(path, newline='', encoding='utf-8') as csvfile:
+    with open(path2, newline='', encoding='utf-8') as csvfile:
         lector_csv = csv.DictReader(csvfile, delimiter=';')
         for numero_fila, fila in enumerate(lector_csv, start=1):
             try:
-                id = fila['\ufeffid'].lstrip('\ufeff')
+                league = fila['\ufeffLeague'].lstrip('\ufeff')
                 season = fila['Season']
-                league = fila['League']
-                jornada = fila['Jornada']
                 home_team = fila['Home Team']
                 away_team = fila['Away Team']
                 home_goals = fila['Home Goals']
@@ -168,12 +167,152 @@ def cargarPartidoEntrenamientoTemporada19_23():
                 winner = fila['Winner']
                 home_points = fila['Home Points']
                 away_points = fila['Away Points']
+                jornada = fila['Jornada']
                 pe = PartidosEntrenamiento.objects.create(id = id, liga=league, temporada=season, jornada=jornada,equipo_local=home_team, equipo_visitante=away_team, goles_local=home_goals, goles_visitante=away_goals, puntos_local=home_points, puntos_visitante=away_points, winner=winner)
+                id = id+1
             except Exception as e:
                 print(f"Error en la fila {numero_fila}: {e}")
-                # Opcional: Puedes añadir más información de la fila si es necesario
                 print(f"Contenido de la fila {numero_fila}: {fila}")
     return pe
+
+def cargarDatosEntrenamientoAdicionales():
+    numero_partidos = PartidosEntrenamiento.objects.all().count()
+    for i in range(1,numero_partidos+1):
+        partidos_en_rango_ordenados = PartidosEntrenamiento.objects.filter(id__range=(1, i+1)).order_by('id')
+        partido = partidos_en_rango_ordenados.last()
+        local = partido.equipo_local
+        visitante = partido.equipo_visitante
+            
+        goles_ultimos_5_partidos_local_siendo_local = 0
+        goles_ultimos_5_partidos_visitante_siendo_visitante = 0
+        goles_ultimos_5_partidos_equipo_local = 0
+        goles_ultimos_5_partidos_equipo_visitante = 0
+
+        puntos_ultimos_5_partidos_local_siendo_local = 0
+        puntos_ultimos_5_partidos_visitante_siendo_visitante = 0
+        puntos_ultimos_5_partidos_equipo_local = 0
+        puntos_ultimos_5_partidos_equipo_visitante = 0
+
+        goles_puntos_local_siendo_local = 0
+        goles_puntos_visitante_siendo_visitante=0
+        goles_puntos_equipo_local = 0
+        goles_puntos_equipo_visitante=0
+
+        goles_en_contra_ultimos_5_partidos_local_siendo_local = 0
+        goles_en_contra_ultimos_5_partidos_visitante_siendo_visitante = 0
+        goles_en_contra_ultimos_5_partidos_equipo_local = 0
+        goles_en_contra_ultimos_5_partidos_equipo_visitante = 0
+
+        for p in reversed(partidos_en_rango_ordenados):
+
+            if goles_puntos_equipo_local >= 5 and goles_puntos_equipo_visitante >=5 and goles_puntos_local_siendo_local >=5 and goles_puntos_visitante_siendo_visitante >=5:
+                continue
+            else:
+                if p.equipo_local == local and p.equipo_visitante == visitante:
+                    pass
+
+                elif p.equipo_local != local and p.equipo_visitante != visitante:
+                    continue
+
+                elif p.equipo_local == local and p.equipo_visitante != visitante:
+                    if goles_puntos_local_siendo_local <5  and goles_puntos_equipo_local <5:
+                        goles_ultimos_5_partidos_local_siendo_local = goles_ultimos_5_partidos_local_siendo_local + p.goles_local
+                        puntos_ultimos_5_partidos_local_siendo_local = puntos_ultimos_5_partidos_local_siendo_local + p.puntos_local
+                        goles_en_contra_ultimos_5_partidos_local_siendo_local = goles_en_contra_ultimos_5_partidos_local_siendo_local + p.goles_visitante
+
+                        goles_ultimos_5_partidos_equipo_local = goles_ultimos_5_partidos_equipo_local + p.goles_local
+                        puntos_ultimos_5_partidos_equipo_local = puntos_ultimos_5_partidos_equipo_local + p.puntos_local
+                        goles_en_contra_ultimos_5_partidos_equipo_local = goles_en_contra_ultimos_5_partidos_equipo_local + p.goles_visitante
+
+                        goles_puntos_local_siendo_local = goles_puntos_local_siendo_local+1
+                        goles_puntos_equipo_local = goles_puntos_equipo_local +1
+
+                    elif goles_puntos_local_siendo_local <5  and goles_puntos_equipo_local >=5:
+                        goles_ultimos_5_partidos_local_siendo_local = goles_ultimos_5_partidos_local_siendo_local + p.goles_local
+                        puntos_ultimos_5_partidos_local_siendo_local = puntos_ultimos_5_partidos_local_siendo_local + p.puntos_local
+                        goles_en_contra_ultimos_5_partidos_local_siendo_local = goles_en_contra_ultimos_5_partidos_local_siendo_local + p.goles_visitante
+                        goles_puntos_local_siendo_local = goles_puntos_local_siendo_local+1
+
+                    elif goles_puntos_local_siendo_local >=5  and goles_puntos_equipo_local <5:
+                        goles_ultimos_5_partidos_equipo_local = goles_ultimos_5_partidos_equipo_local + p.goles_local
+                        puntos_ultimos_5_partidos_equipo_local = puntos_ultimos_5_partidos_equipo_local + p.puntos_local
+                        goles_en_contra__ultimos_5_partidos_equipo_local = goles_en_contra__ultimos_5_partidos_equipo_local + p.goles_visitante
+
+                        goles_puntos_equipo_local = goles_puntos_equipo_local +1
+                    else:
+                        continue
+
+                elif p.equipo_local != local and p.equipo_visitante == visitante:
+                    if goles_puntos_visitante_siendo_visitante <5 and goles_puntos_equipo_visitante <5:
+                        goles_ultimos_5_partidos_visitante_siendo_visitante = goles_ultimos_5_partidos_visitante_siendo_visitante + p.goles_visitante
+                        puntos_ultimos_5_partidos_visitante_siendo_visitante = puntos_ultimos_5_partidos_visitante_siendo_visitante + p.puntos_visitante
+                        goles_en_contra_ultimos_5_partidos_visitante_siendo_visitante = goles_en_contra_ultimos_5_partidos_visitante_siendo_visitante + p.goles_local
+
+                        goles_ultimos_5_partidos_equipo_visitante = goles_ultimos_5_partidos_equipo_visitante + p.goles_visitante
+                        puntos_ultimos_5_partidos_equipo_visitante = puntos_ultimos_5_partidos_equipo_visitante + p.puntos_visitante
+                        goles_en_contra_ultimos_5_partidos_equipo_visitante = goles_en_contra_ultimos_5_partidos_equipo_visitante + p.goles_local
+                                
+                        goles_puntos_visitante_siendo_visitante = goles_puntos_visitante_siendo_visitante+1
+                        goles_puntos_equipo_visitante = goles_puntos_equipo_visitante+1
+
+                    elif goles_puntos_visitante_siendo_visitante <5 and goles_puntos_equipo_visitante >=5:
+                        goles_ultimos_5_partidos_visitante_siendo_visitante = goles_ultimos_5_partidos_visitante_siendo_visitante + p.goles_visitante
+                        puntos_ultimos_5_partidos_visitante_siendo_visitante = puntos_ultimos_5_partidos_visitante_siendo_visitante + p.puntos_visitante
+                        goles_en_contra_ultimos_5_partidos_visitante_siendo_visitante = goles_en_contra_ultimos_5_partidos_visitante_siendo_visitante + p.goles_local
+                        goles_puntos_visitante_siendo_visitante = goles_puntos_visitante_siendo_visitante+1
+                            
+                    elif goles_puntos_visitante_siendo_visitante >=5 and goles_puntos_equipo_visitante <5:
+                        goles_ultimos_5_partidos_equipo_visitante = goles_ultimos_5_partidos_equipo_visitante + p.goles_visitante
+                        puntos_ultimos_5_partidos_equipo_visitante = puntos_ultimos_5_partidos_equipo_visitante + p.puntos_visitante
+                        goles_en_contra_ultimos_5_partidos_equipo_visitante = goles_en_contra_ultimos_5_partidos_equipo_visitante + p.goles_local
+                            
+                        goles_puntos_equipo_visitante = goles_puntos_equipo_visitante + 1
+                    else:
+                        continue
+
+                elif local == p.equipo_visitante:
+                    if goles_puntos_equipo_local < 5:
+                        goles_ultimos_5_partidos_equipo_local = goles_ultimos_5_partidos_equipo_local + p.goles_visitante
+                        puntos_ultimos_5_partidos_equipo_local = puntos_ultimos_5_partidos_equipo_local + p.puntos_visitante
+                        goles_en_contra_ultimos_5_partidos_equipo_local = goles_en_contra_ultimos_5_partidos_equipo_local + p.goles_local
+
+                        goles_puntos_equipo_local = goles_puntos_equipo_local +1
+                    else:
+                        continue
+                elif visitante == p.equipo_local:
+                    if goles_puntos_equipo_visitante < 5:
+                        goles_ultimos_5_partidos_equipo_visitante = goles_ultimos_5_partidos_equipo_visitante + p.goles_local
+                        puntos_ultimos_5_partidos_equipo_visitante = puntos_ultimos_5_partidos_equipo_visitante + p.puntos_local
+                        goles_en_contra_ultimos_5_partidos_equipo_visitante = goles_en_contra_ultimos_5_partidos_equipo_visitante + p.goles_visitante
+                            
+                        goles_puntos_equipo_visitante = goles_puntos_equipo_visitante + 1
+                    else:
+                        continue
+                
+
+        if goles_puntos_equipo_local < 5 and goles_puntos_equipo_visitante <5 and goles_puntos_local_siendo_local <5 and goles_puntos_visitante_siendo_visitante <5:
+                partido.falta = True
+                partido.save()
+        else:
+            partido.goles_ultimos_5_partidos_local_siendo_local = goles_ultimos_5_partidos_local_siendo_local
+            partido.goles_ultimos_5_partidos_equipo_local = goles_ultimos_5_partidos_equipo_local
+            partido.goles_ultimos_5_partidos_visitante_siendo_visitante = goles_ultimos_5_partidos_visitante_siendo_visitante
+            partido.goles_ultimos_5_partidos_equipo_visitante = goles_ultimos_5_partidos_equipo_visitante
+
+            partido.puntos_ultimos_5_partidos_local_siendo_local = puntos_ultimos_5_partidos_local_siendo_local
+            partido.puntos_ultimos_5_partidos_equipo_local = puntos_ultimos_5_partidos_equipo_local
+            partido.puntos_ultimos_5_partidos_visitante_siendo_visitante = puntos_ultimos_5_partidos_visitante_siendo_visitante
+            partido.puntos_ultimos_5_partidos_equipo_visitante = puntos_ultimos_5_partidos_equipo_visitante
+
+            partido.goles_en_contra_ultimos_5_partidos_local_siendo_local = goles_en_contra_ultimos_5_partidos_local_siendo_local
+            partido.goles_en_contra_ultimos_5_partidos_equipo_local = goles_en_contra_ultimos_5_partidos_equipo_local
+            partido.goles_en_contra_ultimos_5_partidos_visitante_siendo_visitante = goles_en_contra_ultimos_5_partidos_visitante_siendo_visitante
+            partido.goles_en_contra_ultimos_5_partidos_equipo_visitante = goles_en_contra_ultimos_5_partidos_equipo_visitante
+            partido.save()
+                
+
+
+    return "Carga Completada"
 
 def cargarPartidoRealLigaEsp():
     id = 1
--- PlayfulPredictions/Predicciones/urls.py
+++ PlayfulPredictions/Predicciones/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import cargar_Datos_Real, cargar_Datos_Sin_Predecir, cargar_Datos_Entrenamiento
 urlpatterns = [
-    path("entrenamiento/", cargar_Datos_Entrenamiento, name="Cargar_Datos_Entrenamiento"),
-    path("real/", cargar_Datos_Real, name="Cargar_Datos_Real"),
-    path("sin_predecir/", cargar_Datos_Sin_Predecir, name="Cargar_Datos_Sin_Predecir"),
+   # path("entrenamiento/", cargar_Datos_Entrenamiento, name="Cargar_Datos_Entrenamiento"),
+   # path("real/", cargar_Datos_Real, name="Cargar_Datos_Real"),
+    #path("sin_predecir/", cargar_Datos_Sin_Predecir, name="Cargar_Datos_Sin_Predecir"),
 ]
